21fa532f776c62f33fa83aa0ad50ff0fa5fe3c95
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-07-17 13:28:43 +0200

    feat: simplify CORS middleware configuration by reverting to default settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,16 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:5173', 'empower-her-frontend-omega.vercel.app'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      exposedHeaders: [],
-      credentials: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
