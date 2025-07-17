54f69cdb559f66df2f39b816113d3bc279544aab
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-07-17 13:19:23 +0200

    configure CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'empower-her-frontend-omega.vercel.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      exposedHeaders: [],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
